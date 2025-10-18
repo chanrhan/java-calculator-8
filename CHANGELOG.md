a698e15d305fb24ed58d3f485dbc16f2e93ce802
chanrhan <km1104rs@naver.com>
2025-10-18 15:31:44 +0900

    feat(Application): add default I/O and exception handling

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,8 @@
 ### docs(readme): add README docs
 
 구현할 기능에 대한 명세와 절차를 정리하여 README.md 파일에 작성함
+
+### feat(Application): add default I/O and exception handling
+
+- `Console.readline()` 을 활용한 입력 기능 구현
+- `calculate()` 메서드 생성하고 예외 시 예외 출력 후 프로그램 종료하도록 함 
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    public static int calculate(String input) throws IllegalArgumentException{
+
+        return -1;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+
+        try{
+            int result = calculate(input);
+            System.out.println(result);
+        }catch (IllegalArgumentException e) {
+            System.out.println("잘못된 값이 입력되었습니다! : " + e.getMessage());
+        }
+
+        // 스캐너 닫기
+        Console.close();
     }
 }
