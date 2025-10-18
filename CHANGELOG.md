97f794d3f08458a70b2d2b74a26f4ba8cc445408
chanrhan <km1104rs@naver.com>
2025-10-18 16:13:01 +0900

    feat(Application): implement calculate() to sum numbers in a string

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,3 +10,11 @@
 
 - `Console.readline()` 을 활용한 입력 기능 구현
 - `calculate()` 메서드 생성하고 예외 시 예외 출력 후 프로그램 종료하도록 함 
+
+### feat(Application): implement calculate() to sum numbers in a string
+
+문자열 내 숫자를 더해 결과를 반환하는 함수 구현 
+- 입력받은 문자열을 정규식(`[,:]`)으로 잘라 `String[]` 형태의 피연산자 배열 생성
+- 피연산자 배열을 순회하여 `String`요소를 정수로 변환(`parseInt`) 
+  - 문자열을 정수로 변환하지 못한 경우는 `NumberFormatException` 예외 메세지를 `IllegalArgumentException` 에 담아 예외를 발생
+- 변환된 정수는 결과값에 더하고, 함수에서는 최종 결과값을 반환
--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,38 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
+import org.assertj.core.internal.Numbers;
 
 public class Application {
     public static int calculate(String input) throws IllegalArgumentException{
+        if(input.equals("")){
+            return 0;
+        }
+        String[] items = input.split("[,:]");
+        if(items.length == 0){
+           throw new IllegalArgumentException("피연산자가 존재하지 않습니다!");
+        }
 
-        return -1;
+        try{
+            int sum=0;
+            for(String item : items){
+                sum += Integer.parseInt(item);
+            }
+            return sum;
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("피연산자가 숫자 형식이 아닙니다!");
+        }
     }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         try{
             int result = calculate(input);
             System.out.println(result);
         }catch (IllegalArgumentException e) {
-            System.out.println("잘못된 값이 입력되었습니다! : " + e.getMessage());
+            System.out.println(e.getMessage());
         }
 
         // 스캐너 닫기
